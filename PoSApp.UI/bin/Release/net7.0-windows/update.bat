a5d2dc99d3cb8418bcc700383454c8f600b4b98a
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2023-06-21 21:06:04 +0300

    added ignore file

--- PoSApp.UI/bin/Release/net7.0-windows/update.bat
+++ PoSApp.UI/bin/Release/net7.0-windows/update.bat
@@ -1,5 +1,5 @@
 taskkill /f /im PoSApp.exe
 :exit
-msiexec.exe /i "PoSApp.Setup.msi" /QN
+msiexec.exe /i "PoSApp.Setup.msi" /passive /qn
 del "PoSApp.Setup.msi";
 start PoSApp.exe
--- PoSApp.UI/update.bat
+++ PoSApp.UI/update.bat
@@ -1,5 +1,5 @@
 taskkill /f /im PoSApp.exe
 :exit
-msiexec.exe /i "PoSApp.Setup.msi" /QN
+msiexec.exe /i "PoSApp.Setup.msi" 
 del "PoSApp.Setup.msi";
 start PoSApp.exe
